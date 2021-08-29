9c83d0b896090d746c647dd5afd383a77b1f78ea
vino19941995 <vinodhinimohan1994@gmail.com>
2021-08-29 21:12:49 +0530

    Add project files.

---
+++ Small_unit_testing/Unit_test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Small_unit_testing
+{
+    public class Unit_test:IDisposable
+    {
+        public int Addition(int a, int b)
+        {
+           return  a + b;
+            
+        }
+        public int subtraction(int a,int b)
+        {
+            if (a < b)
+            {
+                throw new ArgumentException();
+            }
+            return a - b;
+           
+        }
+        
+
+        public void Dispose()
+        {
+            //todo
+        }
+
+       
+    }
+
+}
---
+++ unittest/unit_test_framework.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+using Small_unit_testing;
+
+namespace unittest
+{
+    [TestFixture]
+    public class unit_test_framework
+    {
+        Unit_test test;
+        [SetUp]
+        public void SetUp()
+        {
+            test = new Unit_test();
+        }
+        
+        [Test]
+        public void Addition_test()
+        {
+            var result = test.Addition(3, 4);
+            Assert.AreEqual(7,result);
+        }
+        [Test]
+        public void subtraction_test()
+        {
+            var rsult = test.subtraction(5, 2);
+            Assert.AreEqual(3,rsult);
+        }
+        [Test,Order(2)]
+        public void subtraction_test_exception()
+        {
+
+            Assert.Catch<SystemException>(() => test.subtraction(1, 3));
+            Assert.Throws<ArgumentException>(() => test.subtraction(1, 3)); ;
+        }
+       
+        [TestCase(2,1, 1),Order(1)]
+        public void sbtraction_test_multiple(int first, int second, int third)
+        {
+            var res = test.subtraction(first,second);
+            Assert.AreEqual(third, res);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            test.Dispose();
+        }
+        }
+    }
